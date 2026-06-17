439b2fe8d0f917f61630ead2c4282e145424f9f6
Venkat-Vardhan <saivenkat1579@gmail.com>
2026-06-17 21:52:38 +0530

    Added the billing-service with grpc

--- patient-service/src/test/java/com/pm/patientservice/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/pm/patientservice/PatientServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.pm.patientservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PatientServiceApplication.class)
 class PatientServiceApplicationTests {
 
 	@Test
