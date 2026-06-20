e5ebd1d04b22cebc9d47d4a75a477427707ae7ab
Venkat-Vardhan <saivenkat1579@gmail.com>
2026-06-20 23:14:49 +0530

    Added the auth login endpoint with jwt

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -2,20 +2,36 @@ package com.pm.authservice.controller;
 
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.dto.LoginResponseDTO;
+import com.pm.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.groups.Default;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class AuthController {
+
+    private final AuthService authService;
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
 
     @Operation(summary = "Generate login token for user")
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@Validated(Default.class) @RequestBody LoginRequestDTO loginRequestDTO){
-        return null;
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
+        if(tokenOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        }
+        String token =tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+
     }
 }
